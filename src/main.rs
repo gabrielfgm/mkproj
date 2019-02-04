327e8326ae9bcd139e86169b6a59a24553abcade
gabrielfgm <gabriel.geisler@graduateinstitute.ch>
2019-02-04 23:20:55 +0200

    converted to using clap for cli arguments

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,26 @@
+extern crate clap;
+
+use clap::{Arg, App};
 use std::fs;
-use std::env;
 use std::path::Path;
 
 fn main() -> std::io::Result<()> {
-    let filename = env::args().nth(1).expect("Must provide a directory name.");
+	let matchargs = App::new("mkproj: CLI Utility for Academic Project Generation")
+		.version("0.1")
+		.author("Gabriel Geisler Mesevage <gabriel.mesevage@gmail.com>")
+		.about("Quickly generate a project directory, git init, and subfolders.")
+		.arg(Arg::with_name("INPUT")
+			.help("The project name")
+			.required(true)
+			.index(1))
+		.get_matches();
+
+
+    let filename = Path::new(matchargs.value_of("INPUT").unwrap());
     fs::create_dir(&filename).expect("Check that you have permission to write here!");
-    fs::create_dir(Path::new(&filename).join("draft"))?;
-    fs::create_dir(Path::new(&filename).join("data"))?;
-    fs::create_dir(Path::new(&filename).join("code"))?;
-    fs::create_dir(Path::new(&filename).join("literature"))?;
+    fs::create_dir(&filename.join("draft"))?;
+    fs::create_dir(&filename.join("data"))?;
+    fs::create_dir(&filename.join("code"))?;
+    fs::create_dir(&filename.join("literature"))?;
     Ok(())
 }
